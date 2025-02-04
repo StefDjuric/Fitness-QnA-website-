ea89af38213c1cc713844cf290bd42b0dadec80d
Stefan Djuric <djuric.stef4n@gmail.com>
2025-02-04 12:55:59 +0100

    Add Username field on signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,7 +5,7 @@ import Button from "@/components/Button/Button";
 
 function Signup() {
     return (
-        <div className="max-container w-full padding-container flexCenter h-screen my-32 lg:my-10">
+        <div className="max-container w-full padding-container flexCenter h-screen my-40 lg:my-24">
             <div className="flex flex-col min-w-[300px] lg:min-w-[400px] gap-8 shadow-2xl rounded-lg p-12">
                 <div className="flex flex-col lg:justify-center gap-4 bold-32 mb-4">
                     <Link href="/">
@@ -35,18 +35,29 @@ function Signup() {
                         </label>
                         <input
                             type="email"
-                            name=""
+                            name="email"
+                            id=""
+                            className="rounded-lg border-2 border-green-90 px-2 py-1"
+                        />
+                    </div>
+                    <div className="flex flex-col gap-4 mb-5">
+                        <label className="regular-18" htmlFor="username">
+                            Username
+                        </label>
+                        <input
+                            type="text"
+                            name="username"
                             id=""
                             className="rounded-lg border-2 border-green-90 px-2 py-1"
                         />
                     </div>
                     <div className="flex flex-col gap-4 mb-10">
-                        <label className="regular-18" htmlFor="email">
+                        <label className="regular-18" htmlFor="password">
                             Password
                         </label>
                         <input
                             type="password"
-                            name=""
+                            name="password"
                             id=""
                             className="rounded-lg border-2 border-green-90 px-2 py-1"
                         />
